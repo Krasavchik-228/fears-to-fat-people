c54783e1f6bfd7a4b470890aac0d703c82395020
VPHLOfficial <govnoed235@gmail.com>
2026-05-07 11:41:56 +0700

    hren vsyakoa

---
+++ Assets/scripts/door.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    public Animator animator;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            animator.SetTrigger("Open");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            animator.SetTrigger("Close");
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
